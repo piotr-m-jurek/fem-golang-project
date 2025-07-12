8e3768dc107a6fb91dab0e5f80d6c8d2919de533
Piotr Malecki-Jurek <piotr.m.jurek@checkhq.com>
2025-07-12 15:19:02 +0200

    connect to database

--- go.mod
+++ go.mod
@@ -1,5 +1,17 @@
 module github.com/piotr-m-jurek/fem-project
 
-go 1.22.5
+go 1.23.0
 
-require github.com/go-chi/chi/v5 v5.2.2 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.2.2 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.14.3 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.3 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgtype v1.14.4 // indirect
+	github.com/jackc/pgx/v4 v4.18.3 // indirect
+	golang.org/x/crypto v0.40.0 // indirect
+	golang.org/x/text v0.27.0 // indirect
+)
--- internal/app/app.go
+++ internal/app/app.go
@@ -1,20 +1,29 @@
 package app
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/piotr-m-jurek/fem-project/internal/api"
+	"github.com/piotr-m-jurek/fem-project/internal/store"
 )
 
 type Application struct {
 	Logger         *log.Logger
 	WorkoutHandler *api.WorkoutHandler
+	DB             *sql.DB
 }
 
 func NewApplication() (*Application, error) {
+	pgDB, err := store.Open()
+
+	if err != nil {
+		return nil, err
+	}
+
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
 	// our stores will go here
@@ -25,6 +34,7 @@ func NewApplication() (*Application, error) {
 	app := &Application{
 		Logger:         logger,
 		WorkoutHandler: workoutHandler,
+		DB:             pgDB,
 	}
 	return app, nil
 }
